78664bb797b0efe1e2a263874727886cad75ffae
greg-ogs <alexorogom@gmail.com>
2024-07-05 13:25:32 -0600

    clean NO. 0

--- CNN.py
+++ CNN.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import PIL
 import tensorflow as tf
-
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
